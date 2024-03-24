ea126216f3101cebcdd2aea561d8ba3d60bb491b
SergioIPG00 <94931115+SergioIPG00@users.noreply.github.com>
2024-03-24 17:48:38 -0500

    Create sonarAnalysis.groovy

---
+++ vars/sonarAnalysis.groovy
@@ -0,0 +1,17 @@
+def call(boolean abortPipeline = false) {
+    timeout(time: 5, unit: 'MINUTES') {
+        try {            
+            sh 'echo "Ejecución de las pruebas de calidad de código"'
+            
+            sleep 30             
+            
+            def sonarQubeResult = "SUCCESS"
+			
+            if (abortPipeline && sonarQubeResult != "SUCCESS") {
+                error "QualityGate de SonarQube no pasó. Abortando el pipeline."
+            }
+        } catch (Exception e) {
+            error "Error en el escaneo de SonarQube: ${e.message}"
+        }
+    }
+}
